863c0138a447b2f78920ff4c694b39a641f6c191
Yeray <yerayzafra1@gmail.com>
2025-11-24 10:35:52 +0100

    Manejo de categoría no encontrada

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -40,7 +40,7 @@ namespace ApiEcommerce.Controllers
             var category = _categoryRepository.GetCategory(id);
             if (category == null)
             {
-                return NotFound();
+                return NotFound($"La categoría con el id {id} no existe");
             }
             var categoryDto = _mapper.Map<CategoryDto>(category);
             return Ok(categoryDto);
