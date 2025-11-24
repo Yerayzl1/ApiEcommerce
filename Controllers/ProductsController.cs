2e4507419f40f27f2de6a62c003fd1e8044e62d3
Yeray <yerayzafra1@gmail.com>
2025-11-24 12:38:02 +0100

    Endpoint: Obtener producto

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -26,5 +26,20 @@ namespace ApiEcommerce.Controllers
             var productsDto = _mapper.Map<List<ProductDto>>(products);
             return Ok(productsDto);
         }
+        [HttpGet("{productId:int}", Name = "GetProduct")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetProduct(int productId)
+        {
+            var product = _productRepository.GetProduct(productId);
+            if (product == null)
+            {
+                return NotFound($"El producto con el id {productId} no existe");
+            }
+            var productDto = _mapper.Map<ProductDto>(product);
+            return Ok(productDto);
+        }
     }
 }
