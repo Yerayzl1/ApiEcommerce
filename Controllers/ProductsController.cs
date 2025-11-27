6dc54062943df8c06ace24b5dc4cd7a43aff46ed
Yeray <yerayzafra1@gmail.com>
2025-11-27 10:08:57 +0100

    Endpoint: Buscar producto por nombre o descripción

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -91,5 +91,21 @@ namespace ApiEcommerce.Controllers
             var productsDto = _mapper.Map<List<ProductDto>>(products);
             return Ok(productsDto);
         }
+
+        [HttpGet("searchProductByNameDescription/{searchTerm}", Name = "SearchProducts")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult SearchProducts(string searchTerm)
+        {
+            var products = _productRepository.SearchProducts(searchTerm);
+            if (products.Count == 0)
+            {
+                return NotFound($"Los productos con el nombre o descripción '{searchTerm}' no existen");
+            }
+            var productsDto = _mapper.Map<List<ProductDto>>(products);
+            return Ok(productsDto);
+        }
     }
 }
